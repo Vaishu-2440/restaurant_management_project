3ec674eed98186785114d3dba5c052d88a7e697f
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-07 20:20:05 +0530

    58614

--- main.py
+++ main.py
@@ -2,45 +2,19 @@ from rest_framework import serializers, generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-# Dummy serializer
-class MenuCategorySerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100)
-
-# Dummy data
-MENU_CATEGORIES = [
-    {"name": "Starters"},
-    {"name": "Main Course"},
-    {"name": "Desserts"},
-]
-
-@api_view(['GET'])
-def get_menu_categories(request):
-    serializer = MenuCategorySerializer(MENU_CATEGORIES, many=True)
-    return Response(serializer.data)
-
-# To run test locally
-if __name__ == "__main__":
-    import json
-    print(json.dumps(MENU_CATEGORIES, indent=2))
-=======
-from rest_framework import serializers, generics
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
 class MenuCategorySerializer(serializers.Serializer):
     name = serializers.CharField(max_length = 100)
 
-MENU_CATEGORIES = [
+menu_categories = [
     {"name" : "Starters"},
     {"name" : "Main Course"},
     {"name" : "Desserts"},
 ]
 
 def get_menu_categories(request):
-    serializer = MenuCategorySerializer(MENU_CATEGORIES, many = True)
+    serializer = MenuCategorySerializer(nenu_categories, many = True)
     return Response(serializer.data)
 
 if __name__ == "__main__":
     import json
-    print (json.dumps(MENU_CATEGORIES, indent = 2))
-
+    print (json.dumps(menu_categories, indent = 2))
