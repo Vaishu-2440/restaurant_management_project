cd0c538266ffe69edbbb45e43f353b5f2b26ca3d
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-07 20:35:58 +0530

    58614

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import django
+import json
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "restaurant_management_settings")
+django.setup()
+
+from home.models import MenuCategory
+categories = MenuCategory.objects.all().values('name')
+
+print(json.dumps(list(categories), indent = 2))
