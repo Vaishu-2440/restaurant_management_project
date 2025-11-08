a271026b49480ab195a461d0861db425cd89fb85
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-08 18:03:53 +0530

    58611

--- orders/models.py
+++ orders/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-from .order_status import OrderStatus   # adjust the import if needed
+from .order_status import OrderStatus
 
 class Order(models.Model):
-    customer_name = models.CharField(max_length=100)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
+    customer_name = models.CharField(max_length = 100)
+    total_amount = models.DecimalField(max_digits = 10, decimal_places = 2)
+    created_at = models.DateTimeField(auto_now_add = True)
 
-    # New field for linking to OrderStatus
-    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True)
+    status = models.ForeignKey(OrderStatus, on_delete = models.SET_NULL, null = True)
 
     def __str__(self):
-        return f"Order by {self.customer_name} - Status: {self.status}"
+        return f"Order by {self.customer_name} - Status : {self.status"}
 
