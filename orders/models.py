7ba2c4a24285096027a6e272ebe5abf758b95b12
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-08 18:11:54 +0530

    58611

--- orders/models.py
+++ orders/models.py
@@ -5,8 +5,11 @@ class Order(models.Model):
     customer_name = models.CharField(max_length = 100)
     total_amount = models.DecimalField(max_digits = 10, decimal_places = 2)
     created_at = models.DateTimeField(auto_now_add = True)
-    status = models.ForeignKey(OrderStatus, on_delete = models.CASCADE, null = True)
+    status = models.ForeignKey(
+        OrderStatus, 
+        on_delete = models.CASCADE, 
+        null = True)
 
     def __str__(self):
-        return f"Order by {self.customer_name} - Status : {self.status}"
+        return f"Order by {self.customer_name}, Status : {self.order_status}"
 
