f5de6edeac4a347ad09be1f9057da2d00e5ed4dd
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-08 19:52:20 +0530

    58611

--- orders/models.py
+++ orders/models.py
@@ -1,16 +1,8 @@
 from django.db import models
-from .order_status import OrderStatus
 
 class Order(models.Model) :
-    customer_name = models.CharField(max_length = 100)
-    total_amount = models.DecimalField(max_digits = 10, decimal_places = 2)
-    created_at = models.DateTimeField(auto_now_add = True)
-    status = models.ForeignKey (
-        OrderStatus, 
-        on_delete = models.CASCADE, 
-        null = True,
-    )
+    name = models.CharField(max_length = 50, unique = True)
 
     def __str__(self):
-         return f"Order : {self.customer_name}, Status : {self.status}"
+         return self.name
 
