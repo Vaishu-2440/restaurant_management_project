26c61b017123960805e9d73999885e71f78c9e95
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-08 18:19:05 +0530

    58611

--- orders/models.py
+++ orders/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from .order_status import OrderStatus
 
-class Order(models.Model):
+class Order (models.Model) :
     customer_name = models.CharField(max_length = 100)
     total_amount = models.DecimalField(max_digits = 10, decimal_places = 2)
     created_at = models.DateTimeField(auto_now_add = True)
-    status = models.ForeignKey(
+    status = models.ForeignKey (
         OrderStatus, 
         on_delete = models.CASCADE, 
         null = True
