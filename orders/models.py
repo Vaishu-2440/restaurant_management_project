13324aa5c2faf7d235b0f091e0c504ece5b37c16
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-08 19:57:17 +0530

    58612

--- orders/models.py
+++ orders/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from orders import PENDING,PROCESSING,COMPLETED,CANCELLED
 
 class Order(models.Model) :
-    name = models.CharField(max_length = 50, unique = True)
+   name = models.ChaField(max_length = 50, unique = True)
 
     def __str__(self):
          return self.name
