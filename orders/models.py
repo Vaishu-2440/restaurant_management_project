c7f3dfde70bad12abd740579682828f74735e9c8
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-09 12:49:41 +0530

    58613

--- orders/models.py
+++ orders/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+
 class Coupon(models.Model) :
    code = models.CharField(max_length = 50, unique = True)
    discount_percentage = models.DecimalField(max_digits = 5, decimal_places = 2)
    is_actice = models.BooleanField(default = True)
    valid_from = models.DateField()
    valid_until = models.DateField()
+   
     def __str__(self):
          return f"{self.code} ({self.discount_percentage}% off)"
 
