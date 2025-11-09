1eedbd02f58fa10f19fdad2eabb76d71ed2a1965
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-09 12:54:21 +0530

    58613

--- orders/views.py
+++ orders/views.py
@@ -9,19 +9,19 @@ class CouponValidationView(APIView):
         code = request.data.get("code")
 
         if not code :
-            return Response({"error": "Coupon code is required."}, status = status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "Coupon code is required." }, status = status.HTTP_400_BAD_REQUEST)
         
         try:
             coupon = Coupon.objects.get(code = code)
         except Coupon.DoesNotExist:
-            return Response({"error": "Invalid coupon code."}, status = status.HTTP_400_BAD_REQUEST)
+            return Response({ "error": "Invalid coupon code." }, status = status.HTTP_400_BAD_REQUEST)
 
         today = timezone.now().date()
         if not coupon.is_active :
-            return Response({"Coupon is not active."}, status = status.HTTP_400_BAD_REQUEST)
+            return Response({ "Coupon is not active." }, status = status.HTTP_400_BAD_REQUEST)
 
-        if not (coupon.valid_from <= today <=coupon.valid_until) :
-            return Response({"error": "Coupon is expired or not yet valid."}, status = status.HTTP_400_BAD_REQUEST)
+        if not (coupon.valid_from <= today <= coupon.valid_until) :
+            return Response({ "error": "Coupon is expired or not yet valid." }, status = status.HTTP_400_BAD_REQUEST)
 
         #Valid Response
         
