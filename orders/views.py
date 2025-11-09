69e5978ee925a682004e60d75bd2ecdbb87576df
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-09 19:04:32 +0530

    58613

--- orders/views.py
+++ orders/views.py
@@ -13,7 +13,8 @@ class CouponValidationView(APIView):
             {
                 "success" : False,
                 "message": "Coupon code is required."
-            }, status = status.HTTP_400_BAD_REQUEST)
+            }, status = status.HTTP_400_BAD_REQUEST
+            )
         
         coupon = Coupon.objects.filter(code = code).first()
         if not coupon :
@@ -21,7 +22,8 @@ class CouponValidationView(APIView):
             {
                 "success" : False,
                 "message": "Invalid coupon code."
-            }, status = status.HTTP_400_BAD_FOUND)
+            }, status = status.HTTP_400_BAD_FOUND
+            )
 
         today = timezone.now().date()
         if not coupon.is_active :
@@ -29,25 +31,29 @@ class CouponValidationView(APIView):
             {
                 "success" : False,
                 "message" : "Coupon is not active."
-            }, status = status.HTTP_403_FORBIDDEN)
+            }, status = status.HTTP_403_FORBIDDEN
+            )
 
         if (today < coupon.valid_from) :
             return Response(
             {
                 "success" : False,
                 "message": "Coupon not yet valid."
-            }, status = status.HTTP_400_BAD_REQUEST)
+            }, status = status.HTTP_400_BAD_REQUEST
+            )
 
         if (today > coupon.valid_until) :
             return Response(
             {
                 "success" : False,
                 "message" : "Coupon is expired."
-            }, status = status.HTTP_410_GONE)
+            }, status = status.HTTP_410_GONE
+            )
             
         return Response(
             {
             "success" : True,
             "code" : coupon.code,
             "discount_percentage" : coupon.discount_percentage
-            }, status = status.HTTP_200_OK)
+            }, status = status.HTTP_200_OK
+            )
