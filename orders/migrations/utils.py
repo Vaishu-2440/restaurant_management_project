150f55dab1ac649db315b00a2c3174aa156ea9d3
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-14 18:57:03 +0530

    60582

--- orders/migrations/utils.py
+++ orders/migrations/utils.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-from .models import DailyOperatingHours
+from .models import DailyOperatingHours 
 
-def get_today_operating_hours() :
-    today = datetime.today().strftime('%A')
-    try : 
+def get_today_operating_hours :
+    today = datetime.today.strftime('%A')
+    try :
         hours = DailyOperatingHours.objects.get(day = today)
-        return(hours.open_time, hours.close_time)
+        return (hours.open_time, hours.close_time)
     except DailyOperatingHours.DoesNotExist :
         return (None, None)
