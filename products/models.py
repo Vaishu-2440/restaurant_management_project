4f9bd9b20d317d3d1a0db5820e836722b18e4849
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-14 18:32:15 +0530

    60581

--- products/models.py
+++ products/models.py
@@ -6,5 +6,7 @@ class Item(models.Model):
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    has_delivery = models.BooleanField(default = True)
+
     def __str__(self):
         return str(self.item_name)
