d7439bbcc944fe4e1b774b95c703d546bda15a0d
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-15 18:52:12 +0530

    60583

--- orders/models.py
+++ orders/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Order(models.Model) :
     user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "orders")
     date = models.DateTimeField(auto_now_add = True)
-    total_price = models.DecimalField(max_digits = 10, decimal_place = 2)
+    total_price = models.DecimalField(max_digits = 10, decimal_places = 2)
 
     def __str__(self):
         return f"Order {self.id} - {self.user.username}"
