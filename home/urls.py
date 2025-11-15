62d47e623aa2794cee756e9918ffb86f377e8f2d
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-15 19:30:46 +0530

    60584

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import MenuCategoryListView
+from .views import FeaturedMenuItemsView
 
 urlpatterns = [
     path('categories/', MenuCategoryListView.as_view(), name = 'menu-categories'),
