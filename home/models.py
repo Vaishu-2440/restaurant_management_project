8a30c515c45ab2bec5e7b8cd5a195a52eb5f59a1
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-15 19:24:26 +0530

    60584

--- home/models.py
+++ home/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
-class MenuCategory(models.Model):
-    name = models.CharField(max_length = 100, unique = True)
+class MenuItem(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete = models.CASECADE, related_name = 'menu_items')
+    name = models.CharField(max_length = 200)
+    description = models.TextField(blank = True, null = True)
+    price = models.DecimalField(max_digits = 10, decimal_places = 2)
+    is_available = models.BooleanField(default = True)
+    is_featured = models.BooleanField(default = True)
+    created_at = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
         return self.name
