2b2737177e071961b2bbe8c91b07fad76459419e
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-15 18:51:28 +0530

    60583

--- orders/models.py
+++ orders/models.py
@@ -1,12 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-class Coupon(models.Model) :
-   code = models.CharField(max_length = 50, unique = True)
-   discount_percentage = models.DecimalField(max_digits = 5, decimal_places = 2)
-   is_actice = models.BooleanField(default = True)
-   valid_from = models.DateField()
-   valid_until = models.DateField()
-   
+class Order(models.Model) :
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "orders")
+    date = models.DateTimeField(auto_now_add = True)
+    total_price = models.DecimalField(max_digits = 10, decimal_place = 2)
+
     def __str__(self):
-         return f"{self.code} ({self.discount_percentage}% off)"
+        return f"Order {self.id} - {self.user.username}"
+
 
