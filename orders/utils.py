8f2f5acca6c21be749d6807c6ac3d12c9652f70f
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-16 19:38:10 +0530

    60739

--- orders/utils.py
+++ orders/utils.py
@@ -1,9 +1,12 @@
 from django.db.models import Sum
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Order
 
 def get_daily_sales_total(date) :
+    try :
+        orders = Order.objects.filter(created_at_date = date)
+        total  = orders.aggregate(total_sum = Sum('total_price'))['total_sum']
+        return total or 0
 
-    orders = Order.objects.filter(created_at_date = date)
-    total = orders.aggregate(total_sum = Sum('total_price'))['total_sum']
-    
-    return total or 0
+    except Exception as e :
+        return 0
