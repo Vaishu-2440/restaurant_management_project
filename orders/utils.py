8e5f8cb4219336c5a987c71d893029e8a36e939c
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-16 19:33:44 +0530

    60739

--- orders/utils.py
+++ orders/utils.py
@@ -2,6 +2,8 @@ from django.db.models import Sum
 from .models import Order
 
 def get_daily_sales_total(date) :
+
     orders = Order.objects.filter(created_at_date = date)
-    total = orders.aggregate(total_sum = Sum(total_price))['total_sum']
+    total = orders.aggregate(total_sum = Sum('total_price'))['total_sum']
+    
     return total or 0
