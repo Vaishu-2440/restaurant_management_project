b9b71d44aa7340448ef66f5fcefc8d03119982fb
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-16 19:31:57 +0530

    60739

--- orders/utils.py
+++ orders/utils.py
@@ -1,12 +1,7 @@
-import string
-import secrets
-from .models import Coupon
+from django.db.models import Sum
+from .models import Order
 
-def generate_coupon_code(length = 10) :
-    characters = string.ascii_uppercase + string.digits
-
-    while True :
-        code = ''.join(secrets.choice(characters) for _ in range(length))
-
-        if not Coupon.objects.filter(code = code).exists() :
-            return code
+def get_daily_sales_total(date) :
+    orders = Order.objects.filter(created_at_date = date)
+    total = orders.aggregate(total_sum = Sum(total_price))['total_sum']
+    return total or 0
