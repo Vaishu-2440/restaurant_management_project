dfa26585c45d7caf48cb1a52182eea80c71d4388
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-17 20:55:46 +0530

    60742

--- home/views.py
+++ home/views.py
@@ -3,19 +3,15 @@ from rest_framework.response import Response
 from rest_framework.pagination import PageNumnberPagination
 from home.models import MenuItem
 from home.serializers import MenuItemSerializer
-
 class MenuItemPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = "page_size"
     max_page_size = 50
-
 class MenuItemSearchViewSet(viewsets.ViewSet) :
     pagination_class = MenuItemPagination
-
     def list(self, request) :
         query = request.GET.get('search', '')
         items = MenuItem.objects.filter(name_icontains = query)
-
         paginator = self.pagination_class()
         paginated_items = paginator.paginate_queryset(items, request)
         serializer = MenuItemSerializer(paginated_items, many = True)
