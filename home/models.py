30b4bc4e019279afa313432d808ff0736e0ed834
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-21 15:22:33 +0530

    60933

--- home/models.py
+++ home/models.py
@@ -1,16 +1,13 @@
-from django.db import models
-from .models import MenuItem
-
-class NutritionalInformation(models.Model):
-    menu_item = models.ForeignKey (
-        MenuItem,
-        on_delete = models.CASCADE,
-        related_name = "nutrition"
-    )
-    calories           =      models.IntegerField()
-    protein_grams      =      models.DecimalField(max_digits = 5, decimal_places = 2)
-    fat_grams          =      models.DecimalField(max_digits = 5, decimal_places = 2)
-    carbohydrate_grams =      models.DecimalField(max_digits = 5, decimal_places = 2)
+class Ingredient(models.Model) :
+    name = models.CharField(max_length = 100)
 
     def __str__(self) :
-        return f"{self.menu_item.name} - {self.calories} kcal"
+        return self.name
+
+class MenuItem(models.Model):
+    name = CharField(max_length = 100)
+    price = models.DecimalField(max_digits = 6, decimal_places = 2)
+    ingredients = models.ManyToManyField(Ingredient, related_name = "menu_items")
+
+    def __str__(self) :
+        return self.name
