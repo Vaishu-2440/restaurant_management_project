8afb7a478b06a750b7b6cb7e9e49534bc37c27ac
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-21 11:39:40 +0530

    60932

--- home/utils.py
+++ home/utils.py
@@ -12,4 +12,4 @@ def is_restaurant_open() :
         opening_time = time(10, 0)
         closing_time = time(23, 0)
         
-    return opeming_time  <= current_time <= closing_time
+    return opening_time  <= current_time <= closing_time
