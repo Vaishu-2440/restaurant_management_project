83d7a893bb738a22ed4634762096dffe31d7adfb
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-21 11:45:51 +0530

    60932

--- home/utils.py
+++ home/utils.py
@@ -1,5 +1,4 @@
 from datetime import datetime, time
-
 def is_restaurant_open() :
     now = datetime.now()
     current_day = now.weekday()
@@ -8,9 +7,7 @@ def is_restaurant_open() :
     if current_day < 5 :
         opening_time = time(9, 0)
         closing_time = time(22, 0)
-    
     else :
         opening_time = time(10, 0)
-        closing_time = time(23, 0)
-        
+        closing_time = time(23, 0)    
     return opening_time <= current_time <= closing_time
