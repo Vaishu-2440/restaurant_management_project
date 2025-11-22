1aaff917f1729ca0af896cefd74be9d7fd864122
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-22 15:35:47 +0530

    60935

--- home/models.py
+++ home/models.py
@@ -1,13 +1,16 @@
-class Ingredient(models.Model) :
-    name = models.CharField(max_length = 100)
+from django.db import models
+import datetime
+
+class DailySpecialManager(models.Manager) :
+    def upcoming(self) :
+        today = datetime.date.today()
+        return super().get_queryset().filter(special_date_gte = today)
+class DailySpecial(models.Model) :
+    name = models.CharField(max_length = 255)
+    description = models.TextField()
+    special_date = models.DateField()
+
+    objects = DailySpecialManager
 
     def __str__(self) :
-        return self.name
-
-class MenuItem(models.Model):
-    name = CharField(max_length = 100)
-    price = models.DecimalField(max_digits = 6, decimal_places = 2)
-    ingredients = models.ManyToManyField(Ingredient, related_name = "menu_items")
-
-    def __str__(self) :
-        return self.name
+        return f"{self.name} - {self.special_date}"
