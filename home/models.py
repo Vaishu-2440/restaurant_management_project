12e4252c8264446f450e4fd55f47d717ae65072e
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-24 17:39:11 +0530

    61053

--- home/models.py
+++ home/models.py
@@ -1,16 +1,19 @@
 from django.db import models
-import datetime
+from django.db import Count
 
-class DailySpecialManager(models.Manager) :
-    def upcoming(self) :
-        today = datetime.date.today()
-        return super().get_queryset().filter(special_date_gte = today)
-class DailySpecial(models.Model) :
-    name = models.CharField(max_length = 255)
-    description = models.TextField()
-    special_date = models.DateField()
+class MenuItemManager(models.Manager) :
+    def get_top_selling_items(self, num_items = 5) :
+        return self.get_queryset().annotate(
+            order_count = Count('orderitem')
+        ).order_by('-ordercount')[:num_items]
+class MenuItem(models.Model) :
+    name = models.CharField(max_length = 200)
+    description = models.TextField(null = True, blank= True)
+    price = models.DecimalField(max_digits = 8, decimal_places = 2)
+    category = models.ForeignKey('Category', on_delete = models.CASCADE)
+    available = models.BooleanField(default = True)
 
-    objects = DailySpecialManager
+    objects = MenuItemManager
 
     def __str__(self) :
-        return f"{self.name} - {self.special_date}"
+        return self.name
