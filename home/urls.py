d58d4cf4dfe0ff0978df988d575a5f12ff442387
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-24 11:14:57 +0530

    61052

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,5 @@ from .views import TableListAPIView, TableDetailAPIView
 
 urlpatterns = [
     path('api/tables/', TableListAPIView.as_view(), name = 'table-list'),
-    path('api/tables/<int : pk>/', TableDetailAPIView.as_view(), name = 'table-detail'),
+    path('api/tables/<int:pk>/', TableDetailAPIView.as_view(), name = 'table-detail'),
 ]
