10ec1334c9d644046ec9c18bba11568889ecf80a
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-24 10:38:26 +0530

    61051

--- home/views.py
+++ home/views.py
@@ -1,21 +1,20 @@
-from rest_framework import viewsets, status
+from rest_framework.views import APIView
+from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAdminUser
-from django.shortcuts import get_object_or_404
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 
-class MenuItemUpdateViewSet(viewsets.Viewset) :
-    permission_classes = [IsAdminUser]
+class MenuItemsByCategoryView(APIView) :
+    def get(self, request) :
+        category_name = request.GET.get('category', None)
 
-    def update(self, request, pk = None) :
-        menu_item = get_object_or_404(MenuItem, pk = pk)
-        serializer = MenuItemSerializer(menu_item, data = request.data, partial = False)
-        if serializer.is_valid() :
-            serializer.save()
-            return Response({
-                    "message" : "MenuItem updated successfully",
-                    "data" : serializer.data
-                }, status = status.HTTP_200_OK)
-        
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        if not category_name :
+            return Response(
+                {"error" : "Category query parameter is required"},
+                status = status.HTTP_404_BAD_REQUEST
+            )
+        menu_items = MenuItem.objects.filter(category_category_name_iexact = category_name)
+
+        serializer = MenuItemSerializer(menu_items, many = True)
+
+        return Response(serializer.data, status = status.HTTP_200_OK)
