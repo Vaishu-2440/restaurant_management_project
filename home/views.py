5ea4c77b2784a4f90cab3f773d8f845922293647
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-24 11:02:25 +0530

    61052

--- home/views.py
+++ home/views.py
@@ -1,20 +1,7 @@
-from rest_framework.views import APIView
-from rest_framework import status
-from rest_framework.response import Response
-from .models import MenuItem
-from .serializers import MenuItemSerializer
+from rest_framework import generics
+from .models import Table
+from .serializers import TableSerializer
 
-class MenuItemsByCategoryView(APIView) :
-    def get(self, request) :
-        category_name = request.GET.get('category', None)
-
-        if not category_name :
-            return Response(
-                {"error" : "Category query parameter is required"},
-                status = status.HTTP_404_BAD_REQUEST
-            )
-        menu_items = MenuItem.objects.filter(category_category_name_iexact = category_name)
-
-        serializer = MenuItemSerializer(menu_items, many = True)
-
-        return Response(serializer.data, status = status.HTTP_200_OK)
+class TableDetailAPIView(generics.RetrieveAPIView) :
+    queryset = Table.obects.all()
+    serializer_class = TableSerializer
