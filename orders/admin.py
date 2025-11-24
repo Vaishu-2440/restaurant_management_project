eeb9059517c73d4ca7284f4366807ba75f9e760b
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-24 18:11:55 +0530

    61054

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+def mark_orders_processed(modeladmin, request, queryset) :
+    queryset.update(status = 'Processed')
+mark_orders_processed.short_description = 'Mark selected orders as Processed.'
+
+class OrderAdmin(admin.ModelAdmin) :
+    list_display = ('id', 'customer', 'status', 'created_at')
+    actions = [mark_orders_processed]
+admin.site.register(order, OrderAdmin)
