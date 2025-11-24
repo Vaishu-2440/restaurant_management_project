a7f979b1c1740a1a1fa0fc2efe63b40c4faffb2d
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-24 17:25:54 +0530

    61052

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
 from rest_framework import generics
 from .models import Table
 from .serializers import TableListAPIView, TableDetailAPIView
+
 class TableListAPIView(generics.ListAPIView) :
     queryset = Table.objects.all()
     serializer_class = TableSerializer
+    
 class TableDetailAPIView(generics.RetrieveAPIView) :
     queryset = Table.objects.all()
     serializer_class = TableSerializer
