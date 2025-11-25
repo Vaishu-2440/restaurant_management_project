df0489074ef66cd96c525bd2698112ecc37e832d
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-25 11:39:10 +0530

    61211

--- orders/models.py
+++ orders/models.py
@@ -1,12 +1,25 @@
-from django.contrib.auth.models import User
 from django.db import models
 
+class OrderManager(models.Model) :
+    def get_active_orders(self) :
+        return self.filter(status__in = ['pending', 'processing'])
+
 class Order(models.Model) :
-    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "orders")
-    date = models.DateTimeField(auto_now_add = True)
-    total_price = models.DecimalField(max_digits = 10, decimal_places = 2)
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('processing', 'Processing'),
+        ('completed', 'Completed'),
+        ('cancelled', ' Cancelled'),
+    )
+
+    cutomer = models.ForeignKey('customers.Customer', on_delete = models.CASCADE, null = True, blank = True)
+    total_amount = models.DecimalField(max_digits = 10, decimal_places = 2)
+    status = models.CharField(max_length = 20, choices = STATUS_CHOICES, default = 'pending')
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    objects = OrderManager()
 
     def __str__(self):
-        return f"Order {self.id} - {self.user.username}"
+        return f"Order #{self.id} - {self.status}"
 
 
