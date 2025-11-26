9f28e045163c7beebead932416f5c9677d3df056
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-26 12:08:27 +0530

    61212

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import CouponValidationView
+from .views import OrderDetailAPIView
 
 urlpatterns = [
-    path ('coupons/validate/', CouponValidationView.as_view(), name = 'coupon-validate'),
+    path ('orders/<int : id>/', OrderDetailAPIView.as_view(), name = 'order-detail'),
 ]
