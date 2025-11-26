bbc64d62dcd1065fceb36b3c3f177f425ec01c82
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-26 17:34:11 +0530

    61213

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from .views import TableListAPIView, TableDetailAPIView
+from .views import AvailableTablesAPIView
 
 urlpatterns = [
     path('api/tables/', TableListAPIView.as_view(), name = 'table-list'),
     path('api/tables/<int:pk>/', TableDetailAPIView.as_view(), name = 'table-detail'),
+    path('api/tables/available/', AvailableTablesAPIView.as_view(), name = 'available_tables_api'),
 ]
