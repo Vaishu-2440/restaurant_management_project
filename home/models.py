aaaac95c377376d39b35efdaff87e8ba4b0e90b2
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-28 11:14:06 +0530

    61282

--- home/models.py
+++ home/models.py
@@ -1,19 +1,4 @@
 from django.db import models
-from django.db import Count
-
-class MenuItemManager(models.Manager) :
-    def get_top_selling_items(self, num_items = 5) :
-        return self.get_queryset().annotate(
-            order_count = Count('orderitem')
-        ).order_by('-ordercount')[:num_items]
-class MenuItem(models.Model) :
-    name = models.CharField(max_length = 200)
-    description = models.TextField(null = True, blank= True)
-    price = models.DecimalField(max_digits = 8, decimal_places = 2)
-    category = models.ForeignKey('Category', on_delete = models.CASCADE)
-    available = models.BooleanField(default = True)
-
-    objects = MenuItemManager()
 
 class Restaurant(models.Model) :
     name = models.CharField(max_length = 255)
