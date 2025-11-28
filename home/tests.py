ffbea41daecd84db84213a8b97bf39406f539237
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-28 11:36:03 +0530

    61283

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from rest_framework..test import APITestCase
+from rest_framework import status
+from home.models import Restaurant
+
+class RestaurantInfoAPITest(APITestCase) :
+    def setup(self) :
+        self.restaurant = Restaurant.object.create(
+            name = 'Test Restaurant',
+            address = '123 Test St',
+            phone_number = '9876543210',
+            email = 'test@gmail.com'
+        )
+    def test_get_restaurant_info(self) :
+        restaurant = self.client.get('/api/restaurant - info/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], self.restaurant.name)
+        self.assertEqual(response.data['address'], self.restaurant.address)
 
 # Create your tests here.
