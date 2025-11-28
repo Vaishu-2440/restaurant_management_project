365115bab87e52907d397d0350c8af303c9fe3ec
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-28 10:46:17 +0530

    61281

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserProfileUpdateAPIView
 
 urlpatterns = [
-    path("profile/update/", UserProfileUpdateAPIView.as_view, name = "profile - update"),
+    path("profile/update/", UserProfileUpdateAPIView.as_view(), name = "profile - update"),
 ]
