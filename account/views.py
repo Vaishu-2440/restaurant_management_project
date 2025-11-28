9f3203ccef69c2173c35702b6836ba6cbe85b08c
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-28 10:12:41 +0530

    61281

--- account/views.py
+++ account/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import UserProfileUpdateSerializer
 
-# Create your views here.
+class UserProfileUpdateAPIView(APIView) :
+    permission_classes = [permissions.IsAuthenticated]
+
+    def put(self, request) :
+        user = request.user
+        serializer = UserProfileUpdateSerilaizer(
+            user, data = request.data, partial = True
+        )
+        if serializer.is_valid() :
+            serializer.save()
+            return Response(
+                {"message" : "Profile updated successfully.", "data" : serializer.data},
+                status = status.HTTP_200_OK
+            )
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST) 
+
