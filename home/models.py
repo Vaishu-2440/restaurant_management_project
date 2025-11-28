33ee7b0acf2d7e5be60ef21dae69d21577a0ab0c
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-11-28 11:12:46 +0530

    61282

--- home/models.py
+++ home/models.py
@@ -19,9 +19,8 @@ class Restaurant(models.Model) :
     name = models.CharField(max_length = 255)
     address = models.TextField()
     contact = models.CharField(max_length = 20)
-
     operating_days = models.CharField(
-        max_length = 100
+        max_length = 100,
         help_text = "Comma-separated list of days(e.g., Mon,Tue,Wed,Thu,Fri,Sat,Sun)",
         blank = True,
         null = True
