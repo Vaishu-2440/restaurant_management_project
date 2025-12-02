1abaa3e515c23972331c89c2544266ca6def230e
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-02 18:10:49 +0530

    61475

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from decimal import Decimal
+from home.models import MenuItem
 
-class PaymentMethod(models.Model) :
-    name = models.CharField(max_length = 50, unique = True)
-    description = models.TextField(blank = True, null = True)
-    is_active = models.BooleanField(default = True)
+class Order(models.Model) :
+    order_id = models.CharField(max_length = 20, unique = True)
+    created_at = models.DateTimeField(auto_now_add)
 
     def __str__(self) :
-        return self.name
+        return self.order_id
+    
+    def calculate_total(self) :
+        total = Decimal('0.00')
+        for item in self.order_items.all() :
+            total += item.price * item.quantity
+        return total
