6b446fb25540024426e18d9af5407f95bd129af9
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-02 17:15:35 +0530

    61474

--- home/utils.py
+++ home/utils.py
@@ -1,5 +1,22 @@
-import re
+import smtp
+from email.mime.text import MIMEText
+from django.conf import settings
 
-def is_valid_phone_number(phone_number : str) -> bool :
-    pattern = r'^(\+\d{1,3}[- ]?)?\d{10,12}$'
-    return bool(re.match(pattern, phone_number))
+def send_custom_email(recipient_email, subject, message_body) :
+    try :
+        msg = MIMEText(message_body)
+        msg["Subject"] = subject
+        msg["From"] = settings.EMAIL_HOST_USER
+        msg["To"] = recipient_email
+
+        with smtplib.SMTP(settings.EMAIL_HOST, settings.EMAIL_PORT) as server :
+            server.starttls()
+            server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD) 
+            server.sendmail(settings.EMAIL_HOST_USER, [recipient_email], msg.as_string())
+
+        return True
+
+    except smtplib.SMTPRecipientsRefused :
+        return "Invalid Recipient email address"
+    except Exception as e :
+        return f"Email sending failed : {str(e)}"
