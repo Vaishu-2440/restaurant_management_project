891b3c219a9bd292579bbfe93d8ed3691694623a
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-03 17:39:06 +0530

    61557

--- orders/utils.py
+++ orders/utils.py
@@ -1,10 +1,6 @@
-import secrets
-import string
-from .models import Order
+from datetime import datetime
 
-def generate_unique_order_id(length : int = 8) :
-    characters = string.ascii_uppercase + string.digits
-    while True :
-        new_id = ''.join(secrets.choice(characters) for _ in range(length)) 
-        if not Order.objects.filter(order_id = new_id).exists() :
-            return new_id
+def format_datetime(dt) :
+    if dt is None :
+        return ""
+    return dt.strftime("%B %d, %Y at %I : %M%p")
