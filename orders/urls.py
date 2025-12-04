111195e24b8c66429da1a4a54c80a1b17f533ab0
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-04 12:32:25 +0530

    61560

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import OrderDetailAPIView
+from .views import PaymentMethodListAPIView
 
 urlpatterns = [
     path ('orders/<int : id>/', OrderDetailAPIView.as_view(), name = 'order-detail'),
+    path ('api/payent - methods/', PaymentMethodListAPIView.as_view(), name = 'payment_methods'),
 ]
