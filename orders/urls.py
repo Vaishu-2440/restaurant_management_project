85c797029792b780e53e66d231b1aa210b9a6ce2
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-04 12:33:34 +0530

    61560

--- orders/urls.py
+++ orders/urls.py
@@ -3,6 +3,6 @@ from .views import OrderDetailAPIView
 from .views import PaymentMethodListAPIView
 
 urlpatterns = [
-    path ('orders/<int : id>/', OrderDetailAPIView.as_view(), name = 'order-detail'),
-    path ('api/payent - methods/', PaymentMethodListAPIView.as_view(), name = 'payment_methods'),
+    path ('orders/<int:id>/', OrderDetailAPIView.as_view(), name = 'order-detail'),
+    path ('api/payment - methods/', PaymentMethodListAPIView.as_view(), name = 'payment_methods'),
 ]
