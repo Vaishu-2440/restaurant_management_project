816c90abad996412379b5a752325e09da2c195db
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-06 10:56:58 +0530

    62046

--- home/models.py
+++ home/models.py
@@ -1,10 +1,17 @@
 from django.db import models
-
+"""
 class ContactFormSubmission(models.Model) :
     name = models.CharField(max_length = 255)
     email = models.EmailField()
     message = models.TextField()
     submitted_at = models.DateTimeField(auto_now_add = True)
+"""
+
+class MenuItem(models.Model) :
+    name = models.CharField(max_lentgh = 100)
+    price = models.DecimalField(max_length = 8, decimal_places = 2)
+    description = models.TextField(blank = True, null = True)
+    is_daily_special = models.BooleanField(default = False)
 
     def __str__(self) :
-        return f"{self.name} - {self.email}"
+        return self.name
