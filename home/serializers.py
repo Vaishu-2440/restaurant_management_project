4711e343dc7d111dcd1b6bbaef9aba87b0a569a8
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-06 10:59:05 +0530

    62046

--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
+from .models import MenuItem
+"""
 from .models import ContactFormSubmission
 
 class ContactFormSubmission(serializers.ModelSerializer) :
     class Meta :
         model = ContactFormSubmission
         fields = ['id', 'email', 'name', 'message', 'submitted_at']
+"""
+
+class DailySpecialSerializer(serializer.ModelSerializer) :
+    class Meta :
+        model = MenuItem
+        fields = ['id', 'name', 'price', 'description]'
