ebe078d1cf1d1979b1004460baac918897000c0a
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-06 17:28:16 +0530

    62048

--- home/utils.py
+++ home/utils.py
@@ -1,6 +1,7 @@
 import smtp
 from email.mime.text import MIMEText
 from django.conf import settings
+from home.models impport MenuItem
 
 def send_custom_email(recipient_email, subject, message_body) :
     try :
@@ -20,3 +21,11 @@ def send_custom_email(recipient_email, subject, message_body) :
         return "Invalid Recipient email address"
     except Exception as e :
         return f"Email sending failed : {str(e)}"
+
+def get_distinct_cuisines() :
+    cuisine_names = (
+        MenuItem.objects.values_list("cuisine_name", flat = True)
+        .distinct()
+        .order_by("cuisine_name")
+    )
+    return list(cuisine_name)
