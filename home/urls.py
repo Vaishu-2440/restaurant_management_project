cd429b6e327ae45fb686f604f4231d1f92f7cbab
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-06 11:05:16 +0530

    62046

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import ContactFormSubmissionView
+"""from .views import ContactFormSubmissionView"""
+from .views import DailySpecialAPIView
 
 urlpatterns = [
-    path('contact/submit/', ContactFormSubmissionView.as_view(), name = 'contact-form-submit'),
+    """path('contact/submit/', ContactFormSubmissionView.as_view(), name = 'contact-form-submit'),"""
+    path('daily-specials/', DailySpecialAPIView.as_view(), name = 'daily-special'),
 ]
