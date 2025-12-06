e2751aa1489acdf923e6c7ba849993c56703c14d
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-06 12:00:48 +0530

    62047

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include
+from rest_framework.routers imprt DefaultRouter
 """from .views import ContactFormSubmissionView"""
 from .views import DailySpecialAPIView
+from .views import MenuCategoryViewSet
+
+router = DefaultRouter()
+router.register(r'menu-categories', MenuCategoryViewSet, basename = 'menu-categories')
 
 urlpatterns = [
     """path('contact/submit/', ContactFormSubmissionView.as_view(), name = 'contact-form-submit'),"""
     path('daily-specials/', DailySpecialAPIView.as_view(), name = 'daily-special'),
+    path('', include(router.urls)),
 ]
