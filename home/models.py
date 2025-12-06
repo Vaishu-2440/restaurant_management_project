9fea8957829315934846810b1b9f4b89e89065de
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-06 11:52:35 +0530

    62047

--- home/models.py
+++ home/models.py
@@ -8,10 +8,13 @@ class ContactFormSubmission(models.Model) :
 """
 
 class MenuItem(models.Model) :
-    name = models.CharField(max_lentgh = 100)
+    name = models.CharField(max_length = 100)
     price = models.DecimalField(max_length = 8, decimal_places = 2)
     description = models.TextField(blank = True, null = True)
     is_daily_special = models.BooleanField(default = False)
+
+class MenuCategory(models.Model) :
+    name = models.CharField(max_length = 100, unique = True)
 
     def __str__(self) :
         return self.name
