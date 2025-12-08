5b077ceb7fae52e76ae8e4e6161ae2d12247fbad
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-08 18:07:24 +0530

    62557

--- home/urls.py
+++ home/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-"""from .views import ContactFormSubmissionView"""
+from home.views import CreateReviewAPIView, MenuItemReviewsAPIView
+"""from rest_framework.routers import DefaultRouter
+from .views import ContactFormSubmissionView
 from .views import DailySpecialAPIView
 from .views import MenuCategoryViewSet
 
 router = DefaultRouter()
-router.register(r'menu-categories', MenuCategoryViewSet, basename = 'menu-categories')
+router.register(r'menu-categories', MenuCategoryViewSet, basename = 'menu-categories')"""
 
 urlpatterns = [
-    """path('contact/submit/', ContactFormSubmissionView.as_view(), name = 'contact-form-submit'),"""
+    """path('contact/submit/', ContactFormSubmissionView.as_view(), name = 'contact-form-submit'),
     path('daily-specials/', DailySpecialAPIView.as_view(), name = 'daily-special'),
-    path('', include(router.urls)),
+    path('', include(router.urls)),"""
+    path('reviews/create', CreateReviewAPIView.as_view(), name = 'create-review'),
+    path('reviews/<int:menu_item_id/', MenuItemReviewsAPIview.as_view(), name = 'menu-items-reviews'),
 ]
