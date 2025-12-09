845add610b319f009a22f2200890e5f105269361
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-09 11:28:50 +0530

    62559

--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import MenuCategory
-"""
-from home.models import UserReview
+"""from home.models import UserReview
 from .models import MenuItem
 from .models import ContactFormSubmission
 
