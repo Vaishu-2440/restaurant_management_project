7afaf477f2e27143737dcf6543af9fa9a3daf155
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-09 14:22:12 +0530

    62560

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import OrderDetailAPIView
+from .views import get_order_status
+"""from .views import OrderDetailAPIView
 from .views import PaymentMethodListAPIView
 from .views import CancelOrderView
 
@@ -7,4 +8,8 @@ urlpatterns = [
     path ('orders/<int:id>/', OrderDetailAPIView.as_view(), name = 'order-detail'),
     path ('api/payment - methods/', PaymentMethodListAPIView.as_view(), name = 'payment-methods'),
     path ("cancel - order/<int:order_id>", CancelOrderView.as_view(), name = 'cancel-order'),
+]
+"""
+urlpatterns = [
+    path('order/<int:order_id>/status', get_order_status, name = 'order-status'),
 ]
