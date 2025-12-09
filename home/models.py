efeee7bf5e1be77c2f6d12672af2f5c4a840d3bf
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-09 10:24:14 +0530

    62558

--- home/models.py
+++ home/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from .models import MenuItem
 """
 class ContactFormSubmission(models.Model) :
     name = models.CharField(max_length = 255)
     email = models.EmailField()
     message = models.TextField()
     submitted_at = models.DateTimeField(auto_now_add = True)
-"""
 
 class MenuItem(models.Model) :
     name = models.CharField(max_length = 100)
@@ -18,3 +19,21 @@ class MenuCategory(models.Model) :
 
     def __str__(self) :
         return self.name
+"""
+class UserReview(models.Model) :
+    user = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        related_name = 'reviews'
+    )
+    menu_item = models.ForeignKey(
+        MenuItem,
+        on_delete = models.CASCADE,
+        related_name = 'reviews'
+    )
+    rating = models.IntegerField()
+    comment = models.TextField()
+    review_date = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self) :
+        return f"{self.user.username} - {self.menu_item.name} - {self.rating}/5"
