b1e814f00270abba1e5da35bb64d51b633a66dd6
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-09 11:28:02 +0530

    62559

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-"""from django.contrib.auth.models import User
+"""
+from django.contrib.auth.models import User
 
 class ContactFormSubmission(models.Model) :
     name = models.CharField(max_length = 255)
