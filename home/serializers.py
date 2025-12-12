c08853992e6ceb53965fae136c1a7d41cdb69210
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-12 12:29:44 +0530

    62917

--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Restaurant
 
-class restaurantSerializer(serializer.ModelSerializer) :
+class RestaurantSerializer(serializer.ModelSerializer) :
     class Meta :
         model = Restaurant
         fields = "__all__"
