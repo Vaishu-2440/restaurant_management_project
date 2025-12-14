e81a8dffa2e3dade7d3fd19df111819507193d52
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-14 17:27:58 +0530

    63583

--- home/models.py
+++ home/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Restaurant(models.Model) :
+class UserReview(models.Model) :
+    user = models.Foreignkey(User, on_delete = models.CASCADE)
+    rating = models.IntegerField()
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self) :
+        return f"{self.user.username} - {self.rating}"
+"""class Restaurant(models.Model) :
     name = models.CharField(max_length = 200)
     address = models.TextField()
     phone_number = models.CharField(max_length = 20)
@@ -24,7 +33,7 @@ class Restaurant(models.Model) :
         def __str__(self) :
             return f"{self.day} - {self.restaurant.name}"
 
-"""class LoyaltyProgram(models.Model) :
+    class LoyaltyProgram(models.Model) :
     name = models.CharField(max_length = 100, unique = True)
     points_per_dollar_spent = models.DecimalField(max_digits = 5, decimal_places = 2)
     description = models.TextField()
