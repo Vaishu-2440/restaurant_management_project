895909fe7b17adcdf70b36b2f56ade6e2928af17
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-16 11:10:25 +0530

    63826

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class OrderManager(models.Model) :
+class Order(models.Model) :
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Confirmed', 'Confirmed'),
+        ('Completed', 'Completed'),
+        ('Cancelled', 'Cancelled'),
+    ]
+    order_id = models.CharField(
+        max_length = 20,
+        unique = True,
+        db_index = True
+    )
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    status = models.CharField(
+        max_length = 20,
+        choices = STATUS_CHOICES,
+        default - 'Pending'
+    )
+
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self) :
+        return self.order_id
+        
+"""class OrderManager(models.Model) :
     def with_status(self, status) :
         return self.filter(status = status)
 
@@ -19,3 +44,4 @@ class Order(models.Model) :
 
     def __str__(self) :
         return f"Order #{self.id} - {self.status}"
+"""
