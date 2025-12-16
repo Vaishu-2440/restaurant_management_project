9fac3d76dfc905b736891462000cea1a7ce03460
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-16 20:03:04 +0530

    63827

--- reviews/models.py
+++ reviews/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Review(models.Model) :
-    user = models.Foreigney(User, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
     rating = models.IntegerField()
     comments = models.TextField()
     creayed_at = models.DateTimeField(auto_now_add = True)
