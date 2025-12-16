a342a366eda765a392c1d9965a760aa44669b2e9
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-16 18:50:43 +0530

    63828

--- home/utils.py
+++ home/utils.py
@@ -1,3 +1,12 @@
+import re
+
+def is_valid_email(email) :
+    if not email or isinstance(email, str) :
+        return False
+    
+    email_pattern = r'^[a-ZA-Z0-9_,+-]+@[a-ZA-Z0-9-]+\.[a-ZA-Z0-9-.]+$'
+    return bool(re.match(email_pattern, email))
+
 """import smtp
 from email.mime.text import MIMEText
 from django.conf import settings
@@ -30,7 +39,7 @@ def get_distinct_cuisines() :
         .order_by("cuisine_name")
     )
     return list(cuisine_name)
-"""
+
 def calculate_discount(original_price, discount_percentage) :
     try :
         original_price = float(original_price)
@@ -49,3 +58,4 @@ def calculate_discount(original_price, discount_percentage) :
 
     except (ValueError, TypeError) :
         return {"error" : "Invalid input. Please provide numeric value."}
+"""
