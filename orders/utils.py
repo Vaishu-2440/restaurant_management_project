c8db8d776db1d39df60ef66ca8a1f0e4c7ab2dda
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-18 11:00:38 +0530

    64086

--- orders/utils.py
+++ orders/utils.py
@@ -8,7 +8,7 @@ def calculate_discount(order_total, discount_percentage) :
         if order_total < 0 or discount_percentage < 0 :
             return Decimal('0.00')
         
-        discount_amount = order_total * (discount_percentage / (Decimal('100')))
+        discount_amount = order_total - (discount_percentage / (Decimal('100')))
 
         if discount_amount > order_total :
             return order_total
