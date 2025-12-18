bcb1a07f43e910ede22b3a6438de308d8369a574
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-18 14:33:55 +0530

    64088

--- home/models.py
+++ home/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 
+class MenuCategory(models.Model) :
+    name models.CharField(max_length = 100, unique = True )
+    
+    def __str__(self) :
+        return self.name
+
+class MenuItem(models.Model) :
+    name = models.CharField(max_length = 255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits = 8, decimal_places = 2)
+    category = models.ForeignKey(
+        MenuCategory,
+        on_delete = models.CASCADE,
+        related_name = "menu_items"
+    )
+
+    def __str__(self) :
+        return self.name
+
 """class MenuItem(models.Model) :
     name = models.CharField(max_length = 100)
     price = models.DecimalField(max_digits = 8, decimal_places = 2)
@@ -95,8 +114,7 @@ class Restaurant (models.Model) :
     updated_at = models.DateTimeField(auto_now = True)
 
     def __str__(self) :
-        return self.name
- """   
+        return self.name 
     
 from django.db import models
 
@@ -106,12 +124,12 @@ class Restaurant(models.Model) :
         max_length = 100,
         help_text = "Example: 11:00 AM - 11:00 PM (EST)"
         )
-   """ description = models.TextField(blank = True, null = True)"""
+     description = models.TextField(blank = True, null = True)
 
     def __str__(self) :
         return self.name
 
-"""class ContactFormSubmission(models.Model) :
+class ContactFormSubmission(models.Model) :
     name = models.CharField(max_length = 255)
     email = models.EmailField()
     message = models.TextField()
