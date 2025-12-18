bd2a707326c1e90be05e3e739664cfcb885febf9
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-18 14:18:45 +0530

    64087

--- home/models.py
+++ home/models.py
@@ -96,8 +96,9 @@ class Restaurant (models.Model) :
 
     def __str__(self) :
         return self.name
- """       
-from django.contrib.auth.models import User
+ """   
+    
+from django.db import models
 
 class Restaurant(models.Model) :
     name = models.CharField(max_length = 255)
