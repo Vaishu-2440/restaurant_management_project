3fc33418849b9e3ecaf291f97ed355abc8e09876
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-20 21:41:32 +0530

    64306

--- reviews/models.py
+++ reviews/models.py
@@ -1,11 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import User
+"""from django.contrib.auth.models import User"""
 
 class Review(models.Model) :
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    rating = models.IntegerField()
-    comments = models.TextField()
+    rating = models.PositiveIntegerField()
+    text = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
 
     def __str__(self) :
+        return f" Rating : {self.rating}"
+
+    """user = models.ForeignKey(User, on_delete = models.CASCADE)
+    rating = models.IntegerField()
+    comments = models.TextField()
+
+    def __str__(self) :
         return f"{self.user.username} - {self.rating}"
+"""
