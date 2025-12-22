a2fff3ffad33dad28fad111f64500f23bc3abefe
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-22 13:32:25 +0530

    64475

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from order.models import Order, OrderItem
+from orders.models import Order, OrderItem
 
 class OrderItemSerializer(serializers.ModelSerializer) :
     class Meta :
