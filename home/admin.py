2b32363543faf28515a90501230db14c1c88966b
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-24 20:57:20 +0530

    64574

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Restaurant
+from .models import MenuItem
+
+def make_unavailable(modeladmin, request, queryset) :
+    queryset.update(is_available = False)
+
+    class MenuItemAdmin(admin.ModelAdmin) :
+        list_display = ('name', 'price', 'is_available')
+        list_filter = ('is_available')
+        actions = ['make_unavailable']
+
+"""from .models import Restaurant
 
 class RestaurantAdmin(admin.ModelAdmin) :
     list_display = ('name', 'address', 'phone_number', 'email', 'is_active')
