d6ae6bd31b5e5b5db6860ead6ea09501dbc78721
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-24 12:22:23 +0530

    64573

--- home/serializers.py
+++ home/serializers.py
@@ -5,6 +5,7 @@ class FAQSerializer(serializers.ModelSerializer) :
     class Meta :
         model = FAQ
         fields = ['id', 'question', 'answer']
+        
 
 """
 from .models import OpeningHour
