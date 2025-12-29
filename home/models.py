9d432f3807c08f673137b0bd5a2c4e7e7b8c4121
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-29 10:44:31 +0530

    65007

--- home/models.py
+++ home/models.py
@@ -1,9 +1,15 @@
-"""
+from django.db import models
+
 class MenuItem(models.Model) :
     name = models.CharField(max_length = 100)
     price = models.DecimalField(max_digits = 8, decimal_places = 2)
     cuisine = models.CharField(max_length = 50)
+    is_available = models.BooleanField(default = True)
 
+    def __str__(self) :
+        return self.name
+        
+"""
 class FAQ(models.Model) :
     question = models.CharField(max_length = 255)
     answer = models.TextField()
