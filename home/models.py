3337a807e184eea8b3d3ce4cf922c8cff0c23862
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-29 14:09:26 +0530

    65008

--- home/models.py
+++ home/models.py
@@ -13,6 +13,9 @@ class MenuItem(models.Model) :
 class FAQ(models.Model) :
     question = models.CharField(max_length = 255)
     answer = models.TextField()
+
+
+    
     is_active = models.BooleanField(default = True)
     created_at = models.DateTimeField(auto_now_add = True)
 
