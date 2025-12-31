4992409261944f82fd7a58bffdc6b86a7e9301e1
Vaishu-2440 <vaishnavi7565@gmail.com>
2025-12-31 17:47:18 +0530

    65117

--- home/models.py
+++ home/models.py
@@ -202,7 +202,6 @@ class MenuItem(models.Model) :
     def __str_(self) :
         return self.name
 """
-
 from django.db import models
 
 class MenuCategory(models.Model) :
@@ -220,14 +219,13 @@ class MenuItem(models.Model) :
 
     def __str__(self) :
         return self.name
-
+"""
 class MenuCategory(models.Model) :
     name = models.CharField(max_length = 100, unique = True)
 
     def __str__(self) :
         return self.name
 
-"""
 class UserReview(models.Model) :
     user = models.ForeignKey(
         User,
