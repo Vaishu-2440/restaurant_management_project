a18b59e454640fce1b26e049cdc9abcfb4b1bded
Vaishu-2440 <vaishnavi7565@gmail.com>
2026-01-06 21:45:45 +0530

    65401

--- home/models.py
+++ home/models.py
@@ -5,9 +5,9 @@ class MenuCategory(models.Model) :
     name = models.CharField(max_length = 100, unique = True)
 
     def __str__(self) :
-        return self.name
-        
+        return self.name    
 """
+
 from django.db import models
 
 class Table(models.Model) :
