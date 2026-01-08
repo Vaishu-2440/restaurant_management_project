48b163fcc76bd01960d096bffeea9492673a0d70
Vaishu-2440 <vaishnavi7565@gmail.com>
2026-01-08 17:33:06 +0530

    65441

--- home/models.py
+++ home/models.py
@@ -1,14 +1,29 @@
 from django.db import models
 
 class Cuisine(models.Model) :
+    name = models.CharField(max_length = 200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits = 8, decimal_places = 2)
+    cuisine = models.ForeignKey(
+        Cuisine,
+        on_delete = models.SET_NULL,
+        null = True,
+        blank = True
+    )
+
+    def __str__(self) :
+        return self.name
+
+"""
+from django.db import models
+
+class Cuisine(models.Model) :
     name = models.CharField(max_length = 100)
 
     def __str__(self) :
         return self.name
 
 
-
-"""
 from django.db import models
 
 class Restaurant(models.Model) :
