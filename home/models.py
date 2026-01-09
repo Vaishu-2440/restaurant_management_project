cc5c81d5b8522d5de89f4a82d5b6ec29b9cc91da
Vaishu-2440 <vaishnavi7565@gmail.com>
2026-01-09 21:46:36 +0530

    65478

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+"""
 from django.db import models
 
 class Cuisine(models.Model) :
@@ -6,7 +7,7 @@ class Cuisine(models.Model) :
     def __str__(self) :
         return self.name
 
-""" from .models import Cuisine
+ from .models import Cuisine
 
 class Cuisine(models.Model) :
     name = models.CharField(max_length = 200)
@@ -295,25 +296,29 @@ class MenuCategory(models.Model) :
 
     def __str__(self) :
         return self.name
+"""
+from django.db import models
+from django.contrib.auth.models import User
+
+class MenuItem(models.Model) :
+    name = models.CharField(max_length = 255)
 
 class UserReview(models.Model) :
     user = models.ForeignKey(
         User,
         on_delete = models.CASCADE,
-        related_name = 'reviews'
     )
     menu_item = models.ForeignKey(
         MenuItem,
         on_delete = models.CASCADE,
-        related_name = 'reviews'
     )
     rating = models.IntegerField()
     comment = models.TextField()
-    review_date = models.DateTimeField(auto_now_add = True)
+    created_at = models.DateTimeField(auto_now_add = True)
 
     def __str__(self) :
-        return f"{self.user.username} - {self.menu_item.name} - {self.rating}/5"
-
+        return f"{self.user.username} - {self.menu_item.name} Review"
+"""
 from django.db import models
 
 class Reservation(models.Model) :
