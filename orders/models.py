80a8f1cd4ac34751211a9e4bb487eae3597b4f25
Vaishu-2440 <vaishnavi7565@gmail.com>
2026-01-12 17:41:35 +0530

    65522

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+class Discount(models.Model) :
+    code = models.CharField(max_length = 50, unique = True)
+    percentage = models.DecimalField(max_digits = 5, decimal_places = 2)
+    start_date = models.DateField()
+    end_datee = models.DateField()
+    is_active = models.BooleanField(default = True)
+
+    def __str__(self) :
+        return f"{self.code} ({self.percentage}% off)"
+
+"""
+from django.db import models
+
 class DeliveryZone(models.Model) :
     name = models.CharField(max_length = 100, unique = True)
     min_order_amount = models.DecimalField(max_digits = 10, decimal_places = 2)
@@ -8,8 +21,7 @@ class DeliveryZone(models.Model) :
 
     def __str__(self) :
         return self.name
-
-"""
+"
 from django.db import models
 
 class Order(models.Model) :
