70b96025a0a6ccd6075e440741386baf19242de7
Vaishu-2440 <vaishnavi7565@gmail.com>
2026-01-21 19:35:51 +0530

    65752

--- account/models.py
+++ account/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+from django.conf import settings
+
+class CustomerProfile(models.Model) :
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE,
+        related_name ="customer_profile"
+    )
+    phone_number = models.CharField(max_length = 20, blank = True, null = True)
+    delivery_address = models.TextField(blank = True, null = True)
+
+    def __str__(self) :
+        return f"{self.user.username}'s Customer Profile"
+
+"""
+from django.db import models
 from django.contrib.auth.models import User
 
 CUISINE_CHOICES = (
@@ -7,3 +23,4 @@ CUISINE_CHOICES = (
     ('asian', 'Asian'),
     ('vegetarian', 'Vegetarian'),
 )
+"""
