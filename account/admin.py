7103f31027e04955cdbda77089f5299b502e9237
Vaishu-2440 <vaishnavi7565@gmail.com>
2026-02-02 21:19:52 +0530

    Update admin.py

--- account/admin.py
+++ account/admin.py
@@ -4,7 +4,8 @@ from .models import LoyaltyProgram
 class LoyaltyProgramAdmin(admin.ModelAdmin) :
   list_display = ('name', 'points_per_dollar_spent', 'is_active')
   list_filter = ('is_active',)
-  search_filters = ('name',)
+  search_fields = ('name',)
   
 admin.site.register(LoyaltyProgram, LoyaltyProgramAdmin)
  
+
